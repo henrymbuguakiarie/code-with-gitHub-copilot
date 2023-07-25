af7777c5a1da8180d5c374e64e6cfffd48791212
Henry Mbugua <82150047+henrymbuguakiarie@users.noreply.github.com>
2023-07-25 08:13:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,24 +1,49 @@
-// create web server with express
+// Create web server
+
 const express = require('express');
-const app = express();
-// create body parser
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
+// Create express app
+const app = express();
+
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse requests of content-type - application/json
 app.use(bodyParser.json());
-// create mongoose
-const mongoose = require('mongoose');
-// connect to mongodb
-mongoose.connect('mongodb://localhost:27017/comments');
-// create schema
-const CommentSchema = mongoose.Schema({ name: String, comment: String });
-// create model
-const Comment = mongoose.model('Comment', CommentSchema);
-// create route
-app.post('/comments', async (req, res) => {
-    const comment = new Comment(req.body);
-    try {
-        await comment.save();
-        res.send(comment);
-    } catch (error) {
-        res.sendStatus(500);
-    }
-    });
+
+// Enable CORS
+app.use(cors());
+
+// Define port to listen for requests
+const port = process.env.PORT || 4001;
+
+// Create route handler for GET request at /posts/:id/comments
+app.get('/posts/:id/comments', async (req, res) => {
+  // Get comments from query service
+  const comments = await axios.get(`http://localhost:4002/posts/${req.params.id}/comments`);
+
+  // Return comments
+  res.send(comments.data);
+});
+
+// Create route handler for POST request at /posts/:id/comments
+app.post('/posts/:id/comments', async (req, res) => {
+  // Get comment from request body
+  const { content } = req.body;
+
+  // Get comments from query service
+  const comments = await axios.post(`http://localhost:4002/posts/${req.params.id}/comments`, {
+    content
+  });
+
+  // Return comments
+  res.send(comments.data);
+});
+
+// Listen for requests
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
